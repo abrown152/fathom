53901bacf2f6a0f8ae4f85823b81348ae3bf3bb5
Alysia Brown <alysiacbrown@gmail.com>
2016-08-19 15:05:40 -0700

    Obtained Twinword Sentiment Analysis using tweets from Twitter handle submitted.

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,42 +5,33 @@ require 'twitter'
 class RequestsController < ApplicationController
 
   def self.analyze(handle)
-    # Exchange our oauth_token and oauth_token secret for the AccessToken instance.
-    access_token = prepare_access_token
-
     # Replace underscores in Twitter handles in preparation for URL encoding
     handle.gsub!("_", "%5F")
+
+    # Exchange our oauth_token and oauth_token secret for the AccessToken instance.
+    access_token = prepare_access_token
 
     # use the access token as an agent to get the user's tweets
     response = access_token.request(:get, "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + handle + "&count=100")
 
     tweets = JSON.parse(response.body)
 
-    return tweets
-
-
-
-
-    user_text = ""
-
+    # Compile all tweet text into a single string for analysis
+    @user_text = ""
     tweets.each do |tweet|
-      user_text + tweet["text"]
-      #
-      # tweet_array = tweet_text.split(" ")
-      # tweet_array.each do |word|
-      #   if word[0] == "@" || word[0] == "#"
-          # puts word # this is working but hashtag and handle removal isn't
-          # tweet_array.delete(word)
-          # tweet_text = tweet_array.join(" ")
-          # puts tweet_text
-        # end
-      # end
-
+      @user_text = @user_text + " " + tweet["text"]
     end
 
-    tweets.each do |tweet|
-      tweet_string + " " + tweet["text"]
+    # Remove Twitter handles and hashtags from text to be analyzed
+    @word_array = @user_text.split(" ")
+    @word_array.each do |word|
+      if word[0] == "@" || word[0] == "#"
+        @word_array.delete(word)
+      end
     end
+    @user_text = @word_array.join(" ")
+
+    @user_text = @user_text[0...2000]
 
     response = Unirest.get "https://twinword-sentiment-analysis.p.mashape.com/analyze/",
       headers:{
@@ -49,9 +40,11 @@ class RequestsController < ApplicationController
         "Accept" => "application/json"
       },
       parameters:{
-        "text" => tweet_string
+        "text" => @user_text
       }
       @response = response.body["type"]
+
+      return @response
   end
 
 
